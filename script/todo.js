559b45c4365046cf25f138493d4470b273b0299b
Gvl21 <thornyk1@gmail.com>
2023-08-09 23:03:22 +0900

    add todo_localStorage

--- script/todo.js
+++ script/todo.js
@@ -1,21 +1,30 @@
 const toDoForm = document.getElementById('todo-form');
 const toDoInput = document.querySelector('#todo-form input');
 const toDoList = document.getElementById('todo-list');
+let toDos = [];
+const TODOS_KEY = 'todos';
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
 
 function deleteTodo(e) {
   const deleteTarget = e.target.parentElement;
   deleteTarget.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(deleteTarget.id));
+  saveToDos();
 }
 
 function paintToDo(newTodo) {
   const li = document.createElement('li');
+  li.id = newTodo.id;
   const span = document.createElement('span');
   const btn = document.createElement('button');
   btn.textContent = '❌';
   btn.addEventListener('click', deleteTodo);
   li.appendChild(span);
   li.appendChild(btn);
-  span.textContent = newTodo;
+  span.textContent = newTodo.text;
   toDoList.appendChild(li);
 }
 
@@ -23,7 +32,22 @@ function toDoSubmit(e) {
   e.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = '';
-  paintToDo(newTodo);
+  const newTodoObj = {
+    id: Date.now(),
+    text: newTodo,
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
+  saveToDos();
 }
 
 toDoForm.addEventListener('submit', toDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parseToDos = JSON.parse(savedToDos);
+  toDos = parseToDos;
+
+  toDos.forEach(paintToDo);
+}
